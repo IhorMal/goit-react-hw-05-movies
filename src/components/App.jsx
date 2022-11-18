1c7dce6838723d108c75dfc08fabdc7d0e39f57e
Igor <ihormalyuha@gmail.com>
2022-11-18 19:03:40 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,24 @@
 import css from './App.module.css'
 
 import { Home } from 'page/Home';
-import styled from "styled-components";
+// import styled from "styled-components";
 import { Movies } from 'page/Movies';
 import { MovieDetails } from 'page/MovieDetails';
 // import { NotFound } from 'page/NotFound';
-import { Route, NavLink, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { Cast } from './Cast';
 import { Reviews } from './Reviews';
 import { SharedLayout } from './SharedLayout';
 
 
-const StyledLink = styled(NavLink)`
-  color: black;
-   padding-left: 20px;
-   text-decoration: none;
-  &.active {
-    color: red;
-  }
-`;
+// const StyledLink = styled(NavLink)`
+//   color: black;
+//    padding-left: 20px;
+//    text-decoration: none;
+//   &.active {
+//     color: red;
+//   }
+// `;
 
 export const App = () => {
   return (
